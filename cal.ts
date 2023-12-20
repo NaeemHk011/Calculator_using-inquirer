19fcb8296479f0d40be147922f95348f13129745
NaeemHk011 <150380512+NaeemHk011@users.noreply.github.com>
2023-12-20 10:12:59 +0500

    I have uploaded manually

---
+++ cal.ts
@@ -0,0 +1,47 @@
+import inquirer from "inquirer";
+
+const answer: {
+    n1: number,
+    n2: number,
+    operators: string
+
+
+} = await inquirer.prompt([{
+    type: "number",
+    name: "n1",
+    message: "enter your first number"
+},
+{
+    type: "number",
+    name: "n2",
+    message: "enter your second value"
+},
+{
+    type: "list",
+    name: "operators",
+    choices: ["+", "-", "*", "/"],
+    message: "select your operator"
+},
+]);
+const { n1, n2, operators } = answer
+if (n1 && n2 && operators) {
+    if (operators === "+") {
+        console.log(n1 + n2)
+    } else
+        if (operators === "-") {
+            console.log(n1 - n2)
+        } else
+            if (operators === "*") {
+                console.log(n1 * n2)
+            } else
+                if (operators === "/") {
+                    console.log(n1 / n2)
+                }
+
+
+} else {
+    console.log("please enter a valid value")
+}
+
+
+
